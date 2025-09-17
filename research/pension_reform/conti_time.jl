cf0e45a725f8bd979248549c68b9e0759db74d70
KaiJyun Wang <b11303072@ntu.edu.tw>
2025-09-17 12:18:00 -0500

    thm1.15

--- research/pension_reform/conti_time.jl
+++ research/pension_reform/conti_time.jl
@@ -1,53 +1,56 @@
 using Interpolations, LinearAlgebra, Statistics, Distributions
 using CairoMakie, FastGaussQuadrature, Parameters
 
-
-
-function set_parameters(; r = 0.02, σ = 0.1, ρ = 0.02, αe = 0.94,
-                        αb = 1.0, κ = 1.0, γ = 2.0, λy = 0.1,
-                        λe = 0.2, λu = 0.1, yf = 0.001,
-                        max_t = 100f0, max_a = 30f0,
-                        max_y = 5f0, μ = (t -> -2e-5 * t^2 + 1e-3 * t),
+function set_parameters(; r = 0.3, σ = 0.1, ρ = 0.1, αe = 0.94,
+                        αb = 10.0, κ = 5.0, γ = 3.0, λy = 0.01,
+                        λe = 0.02, λu = 0.01, yf = -8,
+                        max_t = 140, max_a = 20, min_y = -8,
+                        max_y = 1, μ = (t -> -2e-5 * t^2 + 1e-3 * t),
                         h = (t -> 1e-4 * exp(0.1 * t) + 1e-6),
                         b = (a -> αb / (1 - γ) * (κ + a)^(1 - γ))
                         )
-        t_grid = range(0, max_t, length = 10000)
-        a_grid = range(0, max_a, length = 100)
-        y_grid = range(-2, 2, length = 100)
+        t_grid = range(0, max_t, length = 2000) 
+        a_grid = range(0, max_a, length = 500)
+        y_grid = range(min_y, max_y, length = 50)
         dt = t_grid[2] - t_grid[1]
         da = a_grid[2] - a_grid[1]
         e, w = gausshermite(30, normalize = true)
         exp_a_grid = repeat(a_grid, 1, length(e))
         exp_e_grid = permutedims(repeat(e, 1, length(a_grid)))
         exp_w = permutedims(repeat(w, 1, length(a_grid)))
-    return (; r, σ, ρ, αe, αb, κ, γ, λy, λe, λu, yf, max_t, max_a, max_y, μ, h, b, t_grid, a_grid, y_grid, dt, da, e, w, exp_a_grid, exp_e_grid, exp_w)
+    return (; r, σ, ρ, αe, αb, κ, γ, λy, λe, λu, yf, max_t, max_a, min_y, max_y, μ, h, b, t_grid, a_grid, y_grid, dt, da, e, w, exp_a_grid, exp_e_grid, exp_w)
 end
 
 p = set_parameters()
 
+# quasi implicit scheme
 function T(Ve, Vu, t; p = p)
-    @unpack r, σ, ρ, αe, αb, κ, γ, λy, λe, λu, yf, max_t, max_a, max_y, μ, h, b, t_grid, a_grid, y_grid, dt, da, e, w, exp_a_grid, exp_e_grid, exp_w = p
+    @unpack r, σ, ρ, αe, αb, κ, γ, λy, λe, λu, yf, max_t, max_a, min_y, max_y, μ, h, b, t_grid, a_grid, y_grid, dt, da, e, w, exp_a_grid, exp_e_grid, exp_w = p
 
     new_Ve = similar(Ve)
     new_Vu = similar(Vu)
-    itp_Ve = linear_interpolation((a_grid, y_grid), Ve, extrapolation_bc = Line())
-
+    itp_Ve = linear_interpolation((a_grid, y_grid), Ve)
+    
     for (j, y) in enumerate(y_grid)
-        bVe_a = vcat(max((Ve[2,j] - Ve[1,j])/da, αe * exp(-γ * y)), (Ve[2:end, j] - Ve[1:end-1, j]) / da)
-        fVe_a = vcat(max((Ve[2,j] - Ve[1,j])/da, αe * exp(-γ * y)), (Ve[3:end, j] - Ve[2:end-1, j]) / da, (Ve[end, j] - Ve[end-1, j]) / da)
-        ve = (h(t) * b.(a_grid) + λy * dropdims(sum(itp_Ve.(exp_a_grid, y .+ μ(t) .+ σ * exp_e_grid) .* exp_w, dims = 2), dims = 2) + λe * max.(Ve[:,j], Vu)) * dt + Ve[:,j]
-        fμea = r * a_grid .+ exp(y) .+ γ / (1 - γ) * (αe ./ fVe_a).^(1/γ)
-        bμea = r * a_grid .+ exp(y) .+ γ / (1 - γ) * (αe ./ bVe_a).^(1/γ)
-        Se = (1 + (ρ + h(t) + λy + λe) * dt) * Diagonal(ones(length(a_grid))) + dt / da * Tridiagonal(min.(0, bμea[2:end]), max.(fμea, 0) - min.(bμea, 0), -max.(fμea[1:end-1], 0))
-        new_Ve[:, j] = Se \ ve
+        fVe_a = max.(vcat((Ve[2:end, j] - Ve[1:end-1, j])/da, (Ve[end, j] - Ve[end-1, j])/da), 1e-50)
+        bVe_a = max.(vcat(αe * (κ + exp(y))^(-γ), (Ve[2:end, j] - Ve[1:end-1, j])/da), 1e-50)
+        Phi_integral = dropdims(sum(itp_Ve.(exp_a_grid, clamp.(y .+ μ(t) .+ σ * exp_e_grid, min_y, max_y)) .* exp_w, dims = 2), dims = 2) 
+        v = (h(t) * b.(a_grid) + λy * Phi_integral + λe * max.(Vu, Ve[:, j])) * dt + Ve[:, j]
+        fμa = r * a_grid .+ exp(y) .+ αe^(1/γ) * γ / (1 - γ) * (fVe_a).^(-1/γ)
+        bμa = r * a_grid .+ exp(y) .+ αe^(1/γ) * γ / (1 - γ) * (bVe_a).^(-1/γ)
+        A = Tridiagonal(- min.(bμa[2:end], 0), - max.(fμa, 0) + min.(bμa, 0), max.(fμa[1:end-1], 0))
+        S = Diagonal(fill((ρ + h(t) + λy + λe) * dt + 1, length(a_grid))) - dt / da * A
+        new_Ve[:, j] = S \ v
     end
-    bVu_a = vcat(max((Vu[2] - Vu[1]) / da, yf^(-γ)), (Vu[2:end] - Vu[1:end-1]) / da)
-    fVu_a = vcat(max((Vu[2] - Vu[1]) / da, yf^(-γ)), (Vu[3:end] - Vu[2:end-1]) / da, (Vu[end] - Vu[end-1]) / da)
-    vu = (h(t) * b.(a_grid) + λu * dropdims(sum(itp_Ve.(exp_a_grid, log(0.1) .+ σ * exp_e_grid) .* exp_w, dims = 2), dims = 2)) * dt + Vu
-    bμua = r * a_grid .+ yf .+ γ / (1 - γ) * (1 ./ bVu_a).^(1/γ)
-    fμua = r * a_grid .+ yf .+ γ / (1 - γ) * (1 ./ fVu_a).^(1/γ)
-    Su = (1 + (ρ + h(t) + λu) * dt) * Diagonal(ones(length(a_grid))) + dt / da * Tridiagonal(min.(0, bμua[2:end]), max.(fμua, 0) - min.(bμua, 0), -max.(fμua[1:end-1], 0))
-    new_Vu = Su \ vu
+    fVu_a = max.(vcat((Vu[2:end] - Vu[1:end-1]) / da, (Vu[end] - Vu[end-1]) / da), 1e-50)
+    bVu_a = max.(vcat((κ + exp(yf))^(-γ), (Vu[2:end] - Vu[1:end-1]) / da), 1e-50)
+    F_integral = dropdims(sum(max.(itp_Ve.(exp_a_grid, clamp.(-8 .+ σ * exp_e_grid, min_y, max_y)), repeat(Vu, 1, length(e))) .* exp_w, dims = 2), dims = 2) 
+    v = (h(t) * b.(a_grid) + λu * F_integral) * dt + Vu
+    fμa = r * a_grid .+ exp(yf) .+ γ / (1 - γ) * (fVu_a).^(-1/γ)
+    bμa = r * a_grid .+ exp(yf) .+ γ / (1 - γ) * (bVu_a).^(-1/γ)
+    A = Tridiagonal(- min.(bμa[2:end], 0), - max.(fμa, 0) + min.(bμa, 0), max.(fμa[1:end-1], 0))
+    S = Diagonal(fill((ρ + h(t) + λu) * dt + 1, length(a_grid))) - dt / da * A
+    new_Vu = S \ v
     return new_Ve, new_Vu
 end
 
@@ -68,20 +71,32 @@ end
 Ve, Vu = solve()
 
 begin
-    f = Figure()
+    f = Figure(size = (800, 400))
     ax1 = Axis(f[1,1], title = "Ve")
     ax2 = Axis(f[1,2], title = "Vu")
     
-    
     for (i, t) in enumerate(p.t_grid)
-        if i % 10000 == 1
-            lines!(ax1, p.a_grid, Ve[i, :, 1], color = RGBf(0.1 + i / length(p.t_grid), 0.0, 0.9 - i / length(p.t_grid)))
-            lines!(ax2, p.a_grid, Vu[i, :], color = RGBf(0.1 + i / length(p.t_grid), 0.0, 0.9 - i / length(p.t_grid)))
+        if i % 50 == 0
+            lines!(ax1, p.a_grid, Ve[i, :, 40], color = RGBf(0.1 + 0.9* i / length(p.t_grid), 0.0, 0.9 - 0.9* i / length(p.t_grid)))
+            lines!(ax2, p.a_grid, Vu[i, :], color = RGBf(0.1 + 0.9* i / length(p.t_grid), 0.0, 0.9 - 0.9* i / length(p.t_grid)))
         end
     end
     
-    #lines!(ax1, p.a_grid, Ve[1, :, 1], color = :teal, linewidth = 2)
-    #lines!(ax2, p.a_grid, Vu[1, :], color = :brown, linewidth = 2)
+    #lines!(ax1, p.a_grid, Ve[600, :, 10], color = :teal, linewidth = 2)
+    #lines!(ax2, p.a_grid, Vu[600, :], color = :brown, linewidth = 2)
     f
 end
+
+begin
+    f = Figure()
+    ax = Axis(f[1,1], title = "Ve - Vu") 
+    for (i, t) in enumerate(p.t_grid)
+        if i % 100 == 0
+            lines!(ax, exp.(p.y_grid), Ve[i, 10, :] .- Vu[i, 10], color = RGBf(0.1 + 0.9* i / length(p.t_grid), 0.0, 0.9 - 0.9* i / length(p.t_grid)))
+        end
+    end
+    f
+end
+
+
 
